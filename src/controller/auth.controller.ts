70a55254d6a906abfeaab8a2211b3d302c1f8e70
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-02-17 23:44:16 +0530

    add login and logout functionality

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -53,3 +53,44 @@ export const signup = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Internal server error" })
     }
 }
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+        }
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            avatar: user.avatar,
+        })
+    } catch (error) {
+        console.error('sigin controller error : ', error);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const logout = (_: Request, res: Response) => {
+    res.cookie("jwt", "",
+        {
+            httpOnly: true,
+            expires: new Date(0), // An alternative to maxAge: 0
+            // sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development"
+        }
+    );
+    res.status(200).json({ message: "Logged out successfully" })
+}

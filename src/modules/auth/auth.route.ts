c91caaa4ea9ceccd5c1120e55fb9765fe4927bf2
nasimkuniyil <md.nasimkuniyil@gmail.com>
2026-02-24 13:36:48 +0530

    refactor

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -2,17 +2,18 @@ import express, { Request, Response } from "express";
 import AuthController from "./auth.controller";
 import AuthService from "./auth.service";
 import userRepo from "../user/user.repo";
-import { protectRoute } from "../../middlewares/auth.middlewares";
+import AuthMiddleware from "../../middlewares/auth.middlewares";
 
 const router = express.Router();
 
 const authService = new AuthService(userRepo)
 const authController = new AuthController(authService)
+const authMiddleware = new AuthMiddleware(userRepo);
 
 router.post('/signup', (req: Request, res: Response) => authController.register(req, res))
 router.post('/login', (req: Request, res: Response) => authController.login(req, res))
 router.post('/logout', (req: Request, res: Response) => authController.logout(req, res))
-router.put('/update-profile', protectRoute, (req: Request, res: Response) => authController.updateProfile(req, res))
+router.put('/update-profile', authMiddleware.protect, (req: Request, res: Response) => authController.updateAvatar(req, res))
 
 export default router;
 
